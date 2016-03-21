63785a19d12ea1643401fd2646545fd247082bbd
samanatuoit <saman.yaghini@uoit.net>
2016-03-20 21:16:16 -0400

    Initial commit

---
+++ src/sample/Server.java
@@ -0,0 +1,18 @@
+package sample;/**
+ * Created by Saman on 3/20/2016.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Server extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
